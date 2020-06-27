0bd0c55037dca1f231775980907dbeba932dc75e
039-إبراهيم الخشتي <e.a.e.alkhashti@gmail.com>
2020-06-27 20:25:24 +0300

    خلصت بعد يهد يهيد

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,34 +1,61 @@
 // السؤال الأول
 // 1
+const name = "ُEbrahim Abdullah Alkhashti"
 
 // 2
 
+let counter = 15
+
 // 3 and 4
 
+console.log(name+" "+" and I am "+counter)
+
 // 5 and 6
+
+counter *= 5
+
+console.log(counter)
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hobbies=["swimming", "scuba-diving", "reading"]
 // 2
+hobbies.push(15)
+hobbies[4]= 15
+console.log(hobbies.length)
 
 // 3
+hobbies.pop()
+console.log(hobbies)
 
 // 4
+let properties = {
+    name: "web",
+    language: "css",
+    
+    
+}
 
 // 5
+properties[2] =  ["Hussain Almousawi", "Nancy Alsharkawi", "Menawer Alazmi", "Rahaf Alenezi", "Moudhi Albannai" ]
 
 // 6
+console.log(properties.name)
+
 
 // 7
+console.log("Hussain Almousawi")
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
 // 1
+properties[3]= "I love apples"
+
 
 // 2
+console.log(properties[3])
 
 // console results => console.png
